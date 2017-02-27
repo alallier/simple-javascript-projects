c0c6728968db932a012f9f38c26775495751cc96
Josh Lewis <joshlewis7390@gmail.com>
2017-02-26 20:34:07 -0500

    Made improvements by refactoring onkeyup into javascript, removed passCheck call, added README.md

---
+++ Dynamic Password Checking/README.md
@@ -0,0 +1,11 @@
+# Dynamic Password Checker
+
+Dynamic Password Checker
+Turns Verify Password box green and alerts user in green font that they have entered the correct password
+Turns Veryify Password box red and alerts user in red font that they have entered the correct password
+
+CSS formats
+JavaScript uses event listeners to check value of each password box and compare as well as changes colors
+HTML builds website
+
+written by Josh Lewis (joshlewis7390@gmail.com)
--- Dynamic Password Checking/scripts.js
+++ Dynamic Password Checking/scripts.js
@@ -3,30 +3,26 @@ var newPassword = document.getElementById('newPassword'),
     alert = document.getElementById('alert'),
     newPasswordCheck = document.getElementById('newPasswordCheck');
 
+newPassword.addEventListener('keyup', passCheck);
+newPasswordCheck.addEventListener('keyup', passCheck);
+
 // Create a function that checks whether or not the passwords match
 function passCheck() {
-  // Prints to console if function is entered
-  console.log('Checked');
-  
   // Passwords do not match, verify password box is red
   if (newPassword.value !== newPasswordCheck.value) {
     newPasswordCheck.style.backgroundColor = 'red';
     alert.style.color = 'red';
     alert.innerHTML = 'The passwords you entered do not match!';
-
-  }
-  // Password box is empty, verify pword box is yellow
-  else if (newPasswordCheck.value == '' || newPassword.value == '') {
-    newPasswordCheck.style.backgroundColor = 'yellow';
-    alert.innerHTML = 'Please enter a password.'
   }
   // Passswords match, verify password box is green
-  else {
+  else if (newPasswordCheck.value === newPassword.value) {
     newPasswordCheck.style.backgroundColor = 'green';
     alert.style.color = 'green';
     alert.innerHTML = 'The passwords you entered match!';
   }
-
+  // Password box is empty, verify pword box is yellow
+  else if (newPasswordCheck.value === '') {
+    newPasswordCheck.style.backgroundColor = 'yellow';
+    alert.innerHTML = 'Please enter a password.'
+  }
 }
-
-passCheck();
