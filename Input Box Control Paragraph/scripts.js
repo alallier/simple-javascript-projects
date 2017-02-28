ac506ad96be03dbbcf8b09c96a1aa61d331a2333
Josh Lewis <joshlewis7390@gmail.com>
2017-02-27 21:27:39 -0500

    Added comments and fixed some white space in html

--- Input Box Control Paragraph/scripts.js
+++ Input Box Control Paragraph/scripts.js
@@ -1,10 +1,10 @@
 // Create a JavaScript document that takes the input from a text box and fills paragraphs with that text
-
+// Variables grab the input box and the paragraphs
 var inText = document.getElementById('inputBox'),
     pList = document.getElementsByTagName('p');
 
+// Event listener uses keyups and a function that sets contents of paragraphs to the value of the input box
 inText.addEventListener('keyup', function() {
-    console.log(pList);
     for (var i = 0; i < pList.length; i++) {
        pList[i].innerHTML = inText.value;
     }
