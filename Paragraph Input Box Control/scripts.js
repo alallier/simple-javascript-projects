d550a601c9866fd135eb2e479f842bfbe3722977
Josh Lewis <joshlewis7390@gmail.com>
2017-03-01 21:12:23 -0500

    Changed class name of paragraphs taking input, renamed section to be more specific of purpose, spaced out paragraph tags, deleted labels' class, created variable to save length of array containing number of checkboxes.

--- Paragraph Input Box Control/scripts.js
+++ Paragraph Input Box Control/scripts.js
@@ -2,13 +2,14 @@
 // Variables grab the input box and the paragraphs
 var inText = document.getElementById('inputBox'),
     pCheck = document.getElementsByName('check'),
-    pList = document.getElementsByClassName('paragraph');
+    checkboxCount = pCheck.length,
+    pList = document.getElementsByClassName('changingParagraph');
 
 // Event listener uses keyups and a function that sets contents of paragraphs to the value of the input box
 inText.addEventListener('keyup', function() {
     // Loops through paragraph tags, uses if to check for whether the checkboxes are checked or not
     // If they are checked, do not print into paragraph, if box is unchecked, print into paragraph
-  for (var i=0; i < pCheck.length; i++) {
+  for (var i=0; i < checkboxCount; i++) {
     if (!pCheck[i].checked) {
       pList[i].innerHTML = inText.value;
     }
