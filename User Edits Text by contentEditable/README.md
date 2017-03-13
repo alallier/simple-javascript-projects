d1e1cfc89fef610c4b3e37ac0957e65a2f45036d
Josh Lewis <joshlewis7390@gmail.com>
2017-03-13 17:39:48 -0400

    Changed folder name to better reflect contents. Fixed formatting on HTML and added README.md.

---
+++ User Edits Text by contentEditable/README.md
@@ -0,0 +1,5 @@
+# User Edits Text
+
+A website that allows for the user to edit the text within a paragraph on the page.
+
+written by Joshua Lewis (lewisjm200@potsdam.edu)
---
+++ User Edits Text by contentEditable/scripts.js
@@ -0,0 +1,20 @@
+var inputParagraph = document.getElementById('inputParagraph'),
+    editButton = document.getElementById('editButton');
+
+editButton.addEventListener('click', function() {
+  if (editButton.value === "Edit") {
+		inputParagraph.style.display = "none";
+
+	  inputParagraph.contentEditable = true;
+
+		editButton.value = "Save";
+	}
+	else if (editButton.value === "Save") {
+
+		inputParagraph.value = inputParagraph.innerHTML;
+
+    inputParagraph.contentEditable = false;
+
+		editButton.value = "Edit";
+	}
+});
