51db6812eaee1311d11704b6c2ed66c957d23838
Josh Lewis <joshlewis7390@gmail.com>
2017-03-14 09:59:08 -0400

    Made a different version of the User Edits Text page that uses JavaScript to hide the paragraph then add an input box to the HTML where the user can edit the contents of the paragraph.

---
+++ User Edits Text by Input Box/scripts.js
@@ -0,0 +1,35 @@
+var userInputForm = document.getElementById('userInputForm'),
+    editButton = document.getElementById('editButton');
+
+editButton.addEventListener('click', function() {
+  var inputParagraph = document.getElementById('inputParagraph'),
+	    paragraphText,
+			inputbox;
+
+	if (editButton.value === "Edit") {
+		inputParagraph.style.display = "none";
+
+    paragraphText = inputParagraph.innerHTML;
+
+    inputParagraph = document.createElement('input');
+		inputParagraph.setAttribute('type', 'text');
+		inputParagraph.setAttribute('id', 'inputbox');
+
+		userInputForm.appendChild(inputParagraph);
+
+		inputParagraph.value = paragraphText;
+
+		editButton.value = "Save";
+	}
+	else if (editButton.value === "Save") {
+		var inputbox = document.getElementById('inputbox');
+
+    inputParagraph.innerHTML = inputbox.value;
+
+		inputbox.style.display = 'none';
+
+		inputParagraph.style.display = 'initial';
+
+		editButton.value = "Edit";
+	}
+});
